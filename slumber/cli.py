4eebd2176b3ee5d324d603cf58df192c717e3504
Craig Pennington <cpennington@volumeintegration.com>
2016-03-15 14:56:09 -0400

    Add a command line argument that sets a sleep timer in minutes (--timer, -t)

--- slumber/cli.py
+++ slumber/cli.py
@@ -17,6 +17,8 @@ def main():
                         help='Turn on debug logging and USR1 breakpoint')
     parser.add_argument('--sounds', '-s', required=True,
                         help='The directory to load sounds from.  It should be organized into numbered directories.')
+    parser.add_argument('--timer', '-t', required=False, type=int,
+                        help='Set a sleep timer in minutes.')
     args = parser.parse_args()
 
     if args.debug:
@@ -32,23 +34,33 @@ def main():
 
     log.info('Slumber - Starting...')
 
-    if args.debug:
-        import code, traceback, signal
+    if args.debug or args.timer:
+        import signal
+        if args.debug:
+            import code, traceback 
+            
+            def debug_interrupt(sig, frame):
+                debug_locals = dict(frame=frame)
+                debug_locals.update(frame.f_globals)
+                debug_locals.update(frame.f_locals)
+                
+                console = code.InteractiveConsole(locals=debug_locals)
+                console.interact("\n".join([
+                    "Signal %d received.  Entering Python shell." % sig,
+                    "-" * 50,
+                    "Traceback:",
+                    ''.join(traceback.format_stack(frame))
+                ]))
+                    
+            signal.signal(signal.SIGUSR1, debug_interrupt)
 
-        def debug_interrupt(sig, frame):
-            debug_locals = dict(frame=frame)
-            debug_locals.update(frame.f_globals)
-            debug_locals.update(frame.f_locals)
-
-            console = code.InteractiveConsole(locals=debug_locals)
-            console.interact("\n".join([
-                "Signal %d received.  Entering Python shell." % sig,
-                "-" * 50,
-                "Traceback:",
-                ''.join(traceback.format_stack(frame))
-            ]))
-
-        signal.signal(signal.SIGUSR1, debug_interrupt)
+        # set a sleep timer in minutes, minimalist
+        if args.timer:
+            # use SystemExit since that's already caught for debug
+            def stexit(sig, frame):
+              raise SystemExit
+            signal.signal(signal.SIGALRM, stexit)
+            signal.alarm(args.timer * 60)
 
     loop = EventLoop.current()
 
